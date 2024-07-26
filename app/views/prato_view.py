fece21f7549d41b0758d7268ec9577816d00c089
LUCAS NÉIA TORRES <lntorres@mppr.mp.br>
2024-07-26 11:36:31 -0300

    adiciona get de um prato

--- app/views/prato_view.py
+++ app/views/prato_view.py
@@ -8,29 +8,36 @@ router = APIRouter(
     tags=["prato"],
 )
 
-@router.get("/", response_model=List[Prato])
-async def read_pratos():
-    PratoList = PratoController.getListaPratos() 
-    return PratoList
-
 @router.post("/", response_model=Prato)
-async def criar_prato(prato: PratoRequest):
+async def adicionarPrato(prato: PratoRequest):
     novo_prato = PratoController.adicionarPrato(prato)
     return novo_prato
 
+@router.get("/{idPrato}", response_model=Prato)
+async def selecionaPrato(idPrato: int):
+    prato = PratoController.getPrato(idPrato)
+    if prato is None:
+        raise HTTPException(status_code=404, detail="Prato não encontrado")
+    return prato
+
 @router.put("/{idPrato}", response_model=Prato)
-async def editar_prato(idPrato: int, novo_prato: PratoRequest):
+async def editarPrato(idPrato: int, novo_prato: PratoRequest):
     prato = PratoController.editarPrato(idPrato, novo_prato)
     if prato is None:
         raise HTTPException(status_code=404, detail="Prato não encontrado")
     return prato
 
 @router.delete("/{idPrato}")
-async def excluir_prato(idPrato: int):
+async def excluirPrato(idPrato: int):
     prato = PratoController.removePrato(idPrato)
     if prato is None:
         raise HTTPException(status_code=404, detail="Prato não encontrado")
     return prato
+
+@router.get("/", response_model=List[Prato])
+async def ObterListaPratos():
+    PratoList = PratoController.getListaPratos() 
+    return PratoList
 
 # @router.get("/informacaoNutricional/{idPrato}", response_model=str)
 # async def read_info_prato(idPrato: int):
